8cfa8d05450ab3d562067be1dd1f911bb3b57a10
Jackson Nolan <jnolan5@mgh.harvard.edu>
2024-07-12 17:43:13 -0400

    Fixed ruff linting in core/array.py. Fixed variable reference in error message.

--- surfa/core/array.py
+++ surfa/core/array.py
@@ -19,7 +19,7 @@ def conform_ndim(arr, ndim):
     """
     arr = np.asarray(arr)
     if arr.ndim > ndim:
-        raise ValueError(f'cannot conform array of shape {arr.shape} to {ndmi}D')
+        raise ValueError(f'cannot conform array of shape {arr.shape} to {ndim}D')
     for _ in range(ndim - arr.ndim):
         arr = np.expand_dims(arr, axis=-1)
     return arr
