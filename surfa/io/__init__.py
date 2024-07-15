1bcaa47027e0a43743db7b15656baf7c5790f33e
Jackson Nolan <jnolan5@mgh.harvard.edu>
2024-07-15 12:15:04 -0400

    Fixed ruff linting for io/__init__.py

--- surfa/io/__init__.py
+++ surfa/io/__init__.py
@@ -1,8 +1,8 @@
-from .utils import check_file_readability
-from .affine import load_affine
-from .framed import load_volume
-from .framed import load_slice
-from .framed import load_overlay
-from .framed import load_warp
-from .labels import load_label_lookup
-from .mesh import load_mesh
+from .utils import check_file_readability as check_file_readability
+from .affine import load_affine as load_affine
+from .framed import load_volume as load_volume
+from .framed import load_slice as load_slice
+from .framed import load_overlay as load_overlay
+from .framed import load_warp as load_warp
+from .labels import load_label_lookup as load_label_lookup
+from .mesh import load_mesh as load_mesh
