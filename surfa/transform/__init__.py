07794efcd4239a40ec89915d41f6568389a05930
Jackson Nolan <jnolan5@mgh.harvard.edu>
2024-07-15 13:53:28 -0400

    Fixed ruff linting in transform/__init__.py. Added 'as x' to imports/

--- surfa/transform/__init__.py
+++ surfa/transform/__init__.py
@@ -1,15 +1,15 @@
-from .space import Space
-from .space import cast_space
+from .space import Space as Space
+from .space import cast_space as cast_space
 
-from .affine import Affine
-from .affine import cast_affine
-from .affine import affine_equal
-from .affine import identity
-from .affine import compose_affine
-from .affine import random_affine
+from .affine import Affine as Affine
+from .affine import cast_affine as cast_affine
+from .affine import affine_equal as affine_equal
+from .affine import identity as identity
+from .affine import compose_affine as compose_affine
+from .affine import random_affine as random_affine
 
-from .geometry import ImageGeometry
-from .geometry import cast_image_geometry
-from .geometry import image_geometry_equal
+from .geometry import ImageGeometry as ImageGeometry
+from .geometry import cast_image_geometry as cast_image_geometry
+from .geometry import image_geometry_equal as image_geometry_equal
 
-from .warp import Warp
+from .warp import Warp as Warp
